9e2677541d098a3707fc416bfe599fe03a98952b
Davidos533 <medvedkov29@gmail.com>
2021-11-30 21:29:35 +0300

    made fourth exercise

---
+++ exercise_4_console/exercise_4_console_task_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace exercise_4_console_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int x;
+
+            n = UserInput("\n\tEnter n:");
+            x= UserInput("\n\tEnter x:");
+
+            Console.WriteLine($"\n\tResult:{foo(x, n)}");
+        }
+        static double foo(double x,int n,int counter=1)
+        {
+            if (counter != n)
+            {
+                return x / (counter + foo(x, n, counter + 1));
+            }
+            else
+            {
+                 return counter + x;
+            }
+        }
+        static int UserInput(string message)
+        {
+            int input=0;
+            do
+            {
+                Console.Write(message);
+                if (int.TryParse(Console.ReadLine(), out input))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("\n\tIncorrect input");
+                }
+            }
+            while (true);
+
+            return input;
+        }
+        
+    }
+}
---
+++ exercise_4_console/exercise_4_console_task_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace exercise_4_console_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summ=0;
+            int binaryDigit = UserInput("\n\tEnter binary number: ");
+            
+            foo(binaryDigit,ref summ);
+        }
+        static void foo(int binaryNumber,ref int summ, int iter = 0)
+        {
+            if (binaryNumber / 10 != 0||binaryNumber==1)
+            {
+                summ += (binaryNumber % 10 * (int)Math.Pow(2, iter));
+                foo(binaryNumber / 10,ref summ,iter+1);
+            }
+            if(iter==0)
+            {
+                Console.WriteLine(summ);
+            }
+        }
+        static int UserInput(string message)
+        {
+            int input = 0;
+            string userInp=String.Empty;
+            do
+            {
+                Console.Write(message);
+                userInp = Console.ReadLine();
+                if (int.TryParse(userInp, out input)&&ChekValToBinary(userInp))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\n\tIncorrect input");
+                }
+            }
+            while (true);
+
+            return input;
+        }
+        static bool ChekValToBinary(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if ((value[i] != '0') && (value[i] != '1'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
