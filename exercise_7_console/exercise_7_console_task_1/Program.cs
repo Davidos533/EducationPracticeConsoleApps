bff13363959dbd499800a765096e5a7777d6e6c8
Davidos533 <medvedkov29@gmail.com>
2021-12-01 22:44:00 +0300

    made seventh exercise

---
+++ exercise_7_console/exercise_7_console_task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace exercise_7_console_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("\n\tEnter string: ");
+            StringBuilder inputString = new StringBuilder(Console.ReadLine());
+
+            Console.Write("\n\tEnter editable substring: ");
+            string editgSybstring =Console.ReadLine();
+
+            Console.Write("\n\tEnter new substring: ");
+            string newSubstring = Console.ReadLine();
+
+            if (inputString.Length > 0&&editgSybstring.Length>0&&newSubstring.Length>0)
+            { 
+                inputString.Replace(editgSybstring, newSubstring);
+            }
+
+            Console.Write($"\n\t{inputString.ToString()}\n");
+        }
+    }
+}
---
+++ exercise_7_console/exercise_7_console_task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercise_7_console_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\tEnter string: ");
+            string inputString = Console.ReadLine();
+
+            string[] words = inputString.Split(' ');
+
+            int longestWordId=0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[longestWordId].Length < words[i].Length)
+                {
+                    longestWordId = i;
+                }
+            }
+
+            Console.WriteLine($"\n\tLongest word: {words[longestWordId]}");
+        }
+    }
+}
