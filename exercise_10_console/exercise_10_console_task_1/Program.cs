1e891cd56c4c01ee362d9e365f1d520a98e010fe
Davidos533 <medvedkov29@gmail.com>
2021-12-03 16:57:01 +0300

    made tenth exercise

---
+++ exercise_10_console/exercise_10_console_task_1/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Text;
+
+namespace exercise_10_console_task_1
+{
+    class Program
+    {
+        static ProgramFileDirectoryManager programFDManager;
+        static void Main(string[] args)
+        {
+            programFDManager = new ProgramFileDirectoryManager();
+            UserInput();
+        }
+        static void UserInput()
+        {
+            StringBuilder data=new StringBuilder();
+            do
+            {
+                Console.Write($"{programFDManager.m_currentManagerDirectory}:~@ ");
+                string buf;
+                if ((buf = ProcessUserInput(Console.ReadLine(), ref data)) == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write($"LOG::{buf}\n\n");
+                    Console.WriteLine($"{data.ToString()}\n");
+                    if (data.Length > 0)
+                    {
+                        data.Clear();
+                    }
+                }
+            } while (true);
+        }
+        static string ProcessUserInput(string userInput,ref StringBuilder data)
+        {
+            string[] inputParts = userInput.Split(' ');
+            if (inputParts.Length >= 1)
+            {
+                switch (inputParts[0])
+                {
+                    case "cd":
+                        {
+                            // cd ..
+                            if (inputParts.Length == 2 && inputParts[1] == "..")
+                            {
+                                return DecryptStatusCode(programFDManager.GoBackDirectory());
+                            }
+                            // cd directory_name
+                            else if(inputParts.Length==2)
+                            {
+                                return DecryptStatusCode(programFDManager.GoToDirectory(inputParts[1]));
+                            }
+                        }break;
+                        // mk dir directory_name
+                    case "mkdir":
+                        {
+                            if (inputParts.Length == 2)
+                            {
+                                return DecryptStatusCode(programFDManager.CreateDirectory(inputParts[1]));
+                            }
+                        }break;
+                    case "ls":
+                        {
+                            // ls
+                            return DecryptStatusCode(programFDManager.ShowAllCurrentDirectoryFD(ref data));
+                        }
+                    case "echo":
+                        {
+                            // echo "text" > filename.extension
+                            if (inputParts.Length >= 4 && inputParts[inputParts.Length - 2] == ">")
+                            {
+                                return DecryptStatusCode(programFDManager.CreateFile(inputParts[inputParts.Length - 1], GetStringFormStringArrayRange(inputParts, 1, inputParts.Length - 3)));
+                            }
+                        }break;
+                    case "cat":
+                        {
+                            // cat file1 >> file2
+                            if (inputParts.Length==4&&inputParts[2]==">>")
+                            {
+                                return DecryptStatusCode(programFDManager.AppendTextToFile(inputParts[3], inputParts[1]));
+                            }
+                            // cat file1
+                            else if(inputParts.Length==2)
+                            { 
+                                return DecryptStatusCode(programFDManager.ReadFile(inputParts[1],ref data));
+                            }
+                        }break;
+                    case "rm":
+                        {
+                            if (inputParts.Length == 2)
+                            {
+                                return DecryptStatusCode(programFDManager.DeleteFileOrDirectory(inputParts[1]));
+                            }
+                        }break;
+
+                }
+            }
+            return "unknown command";
+        }
+        static string GetStringFormStringArrayRange(string[] array, int startIndex, int stopIndex)
+        {
+            StringBuilder returnableData= new StringBuilder();
+
+            try
+            {
+                for (int i = startIndex; i < stopIndex+1; i++)
+                {
+                    returnableData.Append($"{array[i]} ");
+                }
+                return returnableData.ToString();
+            }
+            catch
+            {
+                return "";
+            }
+
+        }
+        static string DecryptStatusCode(ProgramFileDirectoryManager.StatusCode statusCode)
+        {
+            switch(statusCode)
+            {
+                case ProgramFileDirectoryManager.StatusCode.AlreadyCreated:return "already created";
+                case ProgramFileDirectoryManager.StatusCode.DirectoryNotFound: return "directroty not found";
+                case ProgramFileDirectoryManager.StatusCode.FileNotFound: return "file not found";
+                case ProgramFileDirectoryManager.StatusCode.SomeError: return "some error";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyAppended: return "successfully appended";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyCreated: return "successfully created";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyStepped: return "successfully stepped";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyGet: return "successfully get";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyRead: return "successfully read";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyDelete: return "successfully delete";
+                case ProgramFileDirectoryManager.StatusCode.ElementNotFound: return "element not found";
+                default:return "unknown code";
+            }
+        }
+    }
+}
---
+++ exercise_10_console/exercise_10_console_task_1/ProgramFileDirectoryManager.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace exercise_10_console_task_1
+{
+    class ProgramFileDirectoryManager
+    {
+        public enum StatusCode
+        {
+            SuccessfullyCreated,
+            AlreadyCreated,
+            SuccessfullyAppended,
+            SuccessfullyStepped,
+            SuccessfullyGet,
+            SuccessfullyRead,
+            SuccessfullyDelete,
+            SomeError,
+            DirectoryNotFound,
+            FileNotFound,
+            ElementNotFound,
+        }
+        public string m_currentManagerDirectory { get; private set; }
+
+        public ProgramFileDirectoryManager()
+        {
+            m_currentManagerDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+        public StatusCode DeleteFileOrDirectory(string name)
+        {
+            try
+            {
+                if (File.Exists($"{m_currentManagerDirectory}\\{name}"))
+                {
+                    File.Delete($"{m_currentManagerDirectory}\\{name}");
+                    return StatusCode.SuccessfullyDelete;
+                }
+                else if (Directory.Exists($"{m_currentManagerDirectory}\\{name}"))
+                {
+                    Directory.Delete($"{m_currentManagerDirectory}\\{name}",true);
+                    return StatusCode.SuccessfullyDelete;
+                }
+                else
+                {
+                    return StatusCode.ElementNotFound;
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        public StatusCode ReadFile(string filename,ref StringBuilder fileData)
+        {
+            try
+            {
+                if (!File.Exists($"{m_currentManagerDirectory}\\{filename}"))
+                {
+                    return StatusCode.FileNotFound;
+                }
+                else
+                {
+                    fileData=new StringBuilder(File.ReadAllText($"{m_currentManagerDirectory}\\{filename}"));
+                    return StatusCode.SuccessfullyRead;
+                }
+            }
+            catch
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        public StatusCode ShowAllCurrentDirectoryFD(ref StringBuilder directoryEntries)
+        {
+            try
+            {
+                if(m_currentManagerDirectory.Length<3)
+                {
+                    m_currentManagerDirectory += "\\";
+                }
+                foreach (var systemEntry in Directory.GetFileSystemEntries(m_currentManagerDirectory))
+                {
+                    int finalDirectoryPathLength = systemEntry.LastIndexOf('\\');
+                    directoryEntries.Append($"{systemEntry.Substring(finalDirectoryPathLength+1,systemEntry.Length-finalDirectoryPathLength-1)}\n");
+                }
+                return StatusCode.SuccessfullyGet;
+            }
+            catch(Exception ex)
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        public StatusCode GoBackDirectory()
+        {
+            try
+            {
+                int finalDirectoryPathLength = m_currentManagerDirectory.LastIndexOf('\\');
+                if (!Directory.Exists(m_currentManagerDirectory.Substring(0, finalDirectoryPathLength)))
+                {
+                    return StatusCode.DirectoryNotFound;
+                }
+                else
+                {
+                    m_currentManagerDirectory = m_currentManagerDirectory.Substring(0, finalDirectoryPathLength);
+                    return StatusCode.SuccessfullyStepped;
+                }
+            }
+            catch 
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        public StatusCode GoToDirectory(string directory)
+        {
+            try
+            {
+                // если указанна дерикория отсутствует
+                if (!Directory.Exists($"{m_currentManagerDirectory}\\{directory}"))
+                {
+                    return StatusCode.DirectoryNotFound;
+                }
+                else
+                {
+                    m_currentManagerDirectory = $"{m_currentManagerDirectory}\\{directory}";
+                    return StatusCode.SuccessfullyStepped;
+                }
+            }
+            catch
+            {
+                return StatusCode.SomeError;
+            }
+
+        }
+        public StatusCode AppendTextToFile(string editableFile, string appendFile)
+        {
+            try
+            {
+                string readenData=File.ReadAllText($"{m_currentManagerDirectory}\\{appendFile}");
+
+                File.AppendAllText($"{m_currentManagerDirectory}\\{editableFile}",readenData);
+
+                return StatusCode.SuccessfullyAppended;
+            }
+            catch
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        // метод для слздания файла
+        public StatusCode CreateFile(string fileName,string data)
+        {
+            try
+            {
+                // если файл отсутствует
+                if(!File.Exists($"{m_currentManagerDirectory}\\{fileName}"))
+                {
+                    File.Create($"{m_currentManagerDirectory}\\{fileName}").Close();    // создание файла
+                    if (data != String.Empty)
+                    { 
+                        File.AppendAllText($"{m_currentManagerDirectory}\\{fileName}", data);
+                    }
+                    return StatusCode.SuccessfullyCreated;
+                }
+                else
+                { 
+                    return StatusCode.AlreadyCreated;
+                }
+            }
+            catch(Exception ex)
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        // метод для создание дирекории с заданным именем в папке текущей сборки
+        public StatusCode CreateDirectory(string directoryName)
+        {
+            try
+            {
+                // если дикректория отсутствует
+                if (!Directory.Exists($"{m_currentManagerDirectory}\\{directoryName}"))
+                {
+                    Directory.CreateDirectory($"{m_currentManagerDirectory}\\{directoryName}");   // создание директории
+                    return StatusCode.SuccessfullyCreated;
+                }
+                // иначе если директория уже существует
+                else
+                {
+                    return StatusCode.AlreadyCreated;
+                }
+            }
+            catch
+            {
+                return StatusCode.SomeError;
+            }
+
+        }
+    }
+}
